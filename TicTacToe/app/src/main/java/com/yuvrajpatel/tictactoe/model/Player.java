2836f455d0cc40b5f085528742cf42eb7041a1b1
Yuvraj Patel <yuvipatel003@gmail.com>
2020-02-29 21:37:54 -0500

    Created Player.java class.

---
+++ TicTacToe/app/src/main/java/com/yuvrajpatel/tictactoe/model/Player.java
@@ -0,0 +1,12 @@
+package com.yuvrajpatel.tictactoe.model;
+
+public class Player{
+
+    public String name;
+    public String value;
+
+    public Player(String name, String value) {
+        this.name = name;
+        this.value = value;
+    }
+}
