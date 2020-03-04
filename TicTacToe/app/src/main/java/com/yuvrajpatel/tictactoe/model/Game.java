d4b72269d16511472210c4da0eb5536bbaab99f9
Yuvraj Patel <yuvipatel003@gmail.com>
2020-03-03 21:31:05 -0500

    Added functions in Class Game.

--- TicTacToe/app/src/main/java/com/yuvrajpatel/tictactoe/model/Game.java
+++ TicTacToe/app/src/main/java/com/yuvrajpatel/tictactoe/model/Game.java
@@ -1,5 +1,7 @@
 package com.yuvrajpatel.tictactoe.model;
 
+import android.util.Log;
+
 import androidx.lifecycle.MutableLiveData;
 
 public class Game {
@@ -14,6 +16,7 @@ public class Game {
     public Cell[][] cells;
 
     public MutableLiveData<Player> winner = new MutableLiveData<>();
+
     public Game(String playerOne, String playerTwo) {
         cells = new Cell[BOARD_SIZE][BOARD_SIZE];
         player1 = new Player(playerOne, "X");
@@ -23,5 +26,94 @@ public class Game {
 
     public void switchPlayer() {
         currentPlayer = currentPlayer == player1 ? player2 : player1;
+    }
+
+    public boolean hasGameEnded() {
+        if (hasThreeSameHorizontalCells() || hasThreeSameVerticalCells() || hasThreeSameDiagonalCells()) {
+            winner.setValue(currentPlayer);
+            return true;
+        }
+
+        if (isBoardFull()) {
+            winner.setValue(null);
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean hasThreeSameHorizontalCells() {
+        try {
+            for (int i = 0; i < BOARD_SIZE; i++)
+                if (areEqual(cells[i][0], cells[i][1], cells[i][2]))
+                    return true;
+
+            return false;
+        } catch (NullPointerException e) {
+            Log.e(TAG, e.getMessage());
+            return false;
+        }
+    }
+
+    public boolean hasThreeSameVerticalCells() {
+        try {
+            for (int i = 0; i < BOARD_SIZE; i++)
+                if (areEqual(cells[0][i], cells[1][i], cells[2][i]))
+                    return true;
+            return false;
+        } catch (NullPointerException e) {
+            Log.e(TAG, e.getMessage());
+            return false;
+        }
+    }
+
+    public boolean hasThreeSameDiagonalCells() {
+        try {
+            return areEqual(cells[0][0], cells[1][1], cells[2][2]) ||
+                    areEqual(cells[0][2], cells[1][1], cells[2][0]);
+        } catch (NullPointerException e) {
+            Log.e(TAG, e.getMessage());
+            return false;
+        }
+    }
+
+
+    public boolean isBoardFull() {
+        for (Cell[] row : cells)
+            for (Cell cell : row)
+                if (cell == null ||cell.isEmpty())
+                    return false;
+        return true;
+    }
+    /**
+     * 2 cells are equal if:
+     * - Both are none null
+     * - Both have non null values
+     * - both have equal values
+     *
+     * @param cells: Cells to check if are equal
+     * @return
+     */
+    private boolean areEqual(Cell... cells) {
+        if (cells == null || cells.length == 0)
+            return false;
+
+        for (Cell cell : cells)
+            if (cell == null || cell.player.value == null || cell.player.value.length() == 0)
+                return false;
+
+        Cell comparisonBase = cells[0];
+        for (int i = 1; i < cells.length; i++)
+            if (!comparisonBase.player.value.equals(cells[i].player.value))
+                return false;
+
+        return true;
+    }
+
+    public void reset() {
+        player1 = null;
+        player2 = null;
+        currentPlayer = null;
+        cells = null;
     }
 }
