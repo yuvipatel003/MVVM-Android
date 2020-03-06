2780343a85d14e3d861fc946633a65e4f39e9b47
Yuvraj Patel <yuvipatel003@gmail.com>
2020-03-05 19:35:09 -0500

    Added GameViewModel class which extends viewmodel.

---
+++ TicTacToe/app/src/main/java/com/yuvrajpatel/tictactoe/viewmodel/GameViewModel.java
@@ -0,0 +1,38 @@
+package com.yuvrajpatel.tictactoe.viewmodel;
+
+import android.util.ArrayMap;
+
+import com.yuvrajpatel.tictactoe.model.Cell;
+import com.yuvrajpatel.tictactoe.model.Game;
+import com.yuvrajpatel.tictactoe.model.Player;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
+
+import static com.yuvrajpatel.tictactoe.utilities.StringUtilities.stringFromNumbers;
+
+public class GameViewModel extends ViewModel {
+
+    public ArrayMap<String, String> cells;
+    private Game mGame;
+
+    public void init(String player1, String player2) {
+        mGame = new Game(player1, player2);
+        cells = new ArrayMap<>();
+    }
+
+    public void onClickedCellAt(int row, int column) {
+        if (mGame.cells[row][column] == null) {
+            mGame.cells[row][column] = new Cell(mGame.currentPlayer);
+            cells.put(stringFromNumbers(row, column), mGame.currentPlayer.value);
+            if (mGame.hasGameEnded())
+                mGame.reset();
+            else
+                mGame.switchPlayer();
+        }
+    }
+
+    public LiveData<Player> getWinner() {
+        return mGame.winner;
+    }
+}
