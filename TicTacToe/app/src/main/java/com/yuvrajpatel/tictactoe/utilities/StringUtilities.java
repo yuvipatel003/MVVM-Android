1272d1fe26b5fc5033eb099bee1611a513518a2b
Yuvraj Patel <yuvipatel003@gmail.com>
2020-03-07 21:39:02 -0500

    Update StringUtilities.

--- TicTacToe/app/src/main/java/com/yuvrajpatel/tictactoe/utilities/StringUtilities.java
+++ TicTacToe/app/src/main/java/com/yuvrajpatel/tictactoe/utilities/StringUtilities.java
@@ -1,5 +1,7 @@
 package com.yuvrajpatel.tictactoe.utilities;
 
+import android.text.TextUtils;
+
 public class StringUtilities {
 
     public static String stringFromNumbers(int... numbers) {
@@ -11,6 +13,14 @@ public class StringUtilities {
 
     public static boolean isNullOrEmpty(String value) {
         return value == null || value.length() == 0;
+    }
+
+    public static boolean isValidName(String name){
+
+        if(TextUtils.isEmpty(name) || name == null){
+            return false;
+        }
+        return true;
     }
 
 }
